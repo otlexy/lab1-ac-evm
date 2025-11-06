d22919baad77841a372d6cc743849719b8fd5775
otlexy <134061731+otlexy@users.noreply.github.com>
2025-11-07 03:17:46 +0400

    Add files via upload

---
+++ lab6ac/lab6ac1.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+//перевод целой части в двоичную систему
+string dec_to_bin_do(int integer) {
+    string result = "";
+    while (integer > 0) {
+        result = to_string(integer % 2) + result;
+        integer /= 2;
+    } //заполняется строка result с первого элемента
+    return result;
+}
+
+//перевод дробной части в двоичную систему
+string dec_to_bin_posle(double fractional) {
+    string result = "";
+    for (int i = 0; fractional > 0; i++) {
+        fractional *= 2;
+        if (fractional >= 1) {
+            result += "1";
+            fractional -= 1;
+        }
+        else {
+            result += "0";
+        } //с конца заполняется часть после запятой
+    }
+    return result;
+}
+
+//перевод целой части в восьмеричную систему
+string dec_to_oct_do(int integer) {
+    string result = "";
+    while (integer > 0) {
+        result = to_string(integer % 8) + result;
+        integer /= 8;
+    }
+    return result;
+}
+
+//перевод дробной части в восьмеричную систему
+string dec_to_oct_posle(double fractional) {
+    string result = "";
+    for (int i = 0; fractional > 0; i++) {
+        fractional *= 8;
+        int digit = (int)fractional; //записывается целую часть от умножения
+        result += to_string(digit);
+        fractional -= digit; //отнимается целая часть для продолжения работы с дробной частью
+    }
+    return result;
+}
+
+//перевод целой части в шестнадцатеричную систему
+string dec_to_hex_do(int integer) {
+    string result = "";
+    while (integer > 0) {
+        if (integer % 16 == 10) {
+            result = 'A' + result;
+        }
+        else if (integer % 16 == 11) {
+            result = 'B' + result;
+        }
+        else if (integer % 16 == 12) {
+            result = 'C' + result;
+        }
+        else if (integer % 16 == 13) {
+            result = 'D' + result;
+        }
+        else if (integer % 16 == 14) {
+            result = 'E' + result;
+        }
+        else if (integer % 16 == 15) {
+            result = 'F' + result;
+        } //проверка остатка на >= 10
+        else {
+            result = to_string(integer % 16) + result;
+        }
+        integer /= 16;
+    }
+    return result;
+}
+
+//перевод дробной части в шестнадцатеричную систему
+string dec_to_hex_posle(double fractional) {
+    string result = "";
+    for (int i = 0; fractional > 0; i++) {
+        fractional *= 16;
+        int digit = int(fractional);
+        if (digit == 10) {
+            result += 'A';
+        }
+        else if (digit == 11) {
+            result += 'B';
+        }
+        else if (digit == 12) {
+            result += 'C';
+        }
+        else if (digit == 13) {
+            result += 'D';
+        }
+        else if (digit == 14) {
+            result += 'E';
+        }
+        else if (digit == 15) {
+            result += 'F';
+        }
+        else {
+            result += to_string(digit);
+        }
+        fractional -= digit;
+    }
+    return result;
+}
+
+int main() {
+    double number;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число в десятичной системе счисления: ";
+    cin >> number;
+    int integer = int(number); //целая часть
+    double fractional = number - integer; //дробная часть
+    string binary, octal, hex; //строки, в которые будет записываться ответ
+
+    //запись окончательных переводов в различные системы счисления
+    if (fractional == 0) {
+        binary = dec_to_bin_do(integer);
+        octal = dec_to_oct_do(integer);
+        hex = dec_to_hex_do(integer);
+    }
+    else {
+        binary = dec_to_bin_do(integer) + '.' + dec_to_bin_posle(fractional);
+        octal = dec_to_oct_do(integer) + '.' + dec_to_oct_posle(fractional);
+        hex = dec_to_hex_do(integer) + '.' + dec_to_hex_posle(fractional);
+    }
+
+    //вывод ответа
+    cout << "Двоичная СС:  " << binary << '\n';
+    cout << "Восьмеричная СС: " << octal << '\n';
+    cout << "Шестнадцатеричная СС: " << hex << '\n';
+}
---
+++ lab6ac/lab6ac2.cpp
@@ -0,0 +1,191 @@
+﻿#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+//функция возвращает целую часть числа
+string do_zapyat(string number) {
+    string do_zap;
+    for (int i = 0; i < number.length(); i++) {
+        if (number[i] == '.') {
+            break;
+        }
+        do_zap += number[i];
+    }
+    return do_zap;
+}
+
+//функция возвращает дробную часть числа
+string posle_zapyat(string number) {
+    string posle_zap;
+    bool found_dot = false;
+    for (int i = 0; i < number.length(); i++) {
+        if (number[i] == '.') {
+            found_dot = true;
+            continue;
+        }
+        if (found_dot) {
+            posle_zap += number[i];
+        }
+    }
+    return posle_zap;
+}
+
+//функция перевода из двоичной в десятичную СС
+string bin_to_dec(string do_zap, string posle_zap) {
+    long result1 = 0; //в эту переменную записывается результат перевода целой части числа
+    int stepen1 = 1;
+    for (int i = do_zap.length() - 1; i >= 0; i--) {
+        result1 += (do_zap[i] - '0') * stepen1; //записывается результат перевода для цифры (перевод из типа char в int)
+        stepen1 *= 2;
+    } //цикл перевода из 2ой в 10ую для целой части
+    double result2 = 0; //в эту переменную записывается результат перевода дробной части числа
+    double stepen2 = 0.5;
+    for (int i = 0; i < posle_zap.length(); i++) {
+        result2 += (posle_zap[i] - '0') * stepen2;
+        stepen2 /= 2;
+    } //появляется лишний 0. при использовании to_string(result2) на типе double
+    int temp = result2 * pow(10, posle_zap.length()); //переменная, чтобы убрать лишние '0' после запятой
+    string bin_dec;
+    if (result2 == 0) {
+        bin_dec = to_string(result1);
+    }
+    else if (result1 == 0) {
+        bin_dec = "0." + to_string(temp);
+    }
+    else {
+        bin_dec = to_string(result1) + "." + to_string(temp);
+    } //проверка разных случаев для числа
+    return bin_dec;
+}
+
+//функция перевода из восьмеричной в десятичную СС
+string oct_to_dec(string do_zap, string posle_zap) {
+    int result1 = 0;
+    int stepen1 = 1;
+    for (int i = do_zap.length() - 1; i >= 0; i--) {
+        result1 += (do_zap[i] - '0') * stepen1;
+        stepen1 *= 8;
+    }
+    double result2 = 0;
+    double stepen2 = 0.125;
+    for (int i = 0; i < posle_zap.length(); i++) {
+        result2 += (posle_zap[i] - '0') * stepen2;
+        stepen2 /= 8;
+    }
+    int temp = result2 * pow(10, posle_zap.length());
+    string oct_dec; //окончательный результат перевода из 8ой в 10ую
+    if (result2 == 0) {
+        oct_dec = to_string(result1);
+    }
+    else if (result1 == 0) {
+        oct_dec = "0." + to_string(temp);
+    }
+    else {
+        oct_dec = to_string(result1) + "." + to_string(temp);
+    }
+    return oct_dec;
+}
+
+//функция перевода из шестнадцатеричной в десятичную СС
+string hex_to_dec(string do_zap, string posle_zap) {
+    int result1 = 0;
+    int stepen1 = 1;
+    for (int i = do_zap.length() - 1; i >= 0; i--) {
+        int digit;
+        if (do_zap[i] == 'A') {
+            digit = 10;
+        }
+        else if (do_zap[i] == 'B') {
+            digit = 11;
+        }
+        else if (do_zap[i] == 'C') {
+            digit = 12;
+        }
+        else if (do_zap[i] == 'D') {
+            digit = 13;
+        }
+        else if (do_zap[i] == 'E') {
+            digit = 14;
+        }
+        else if (do_zap[i] == 'F') {
+            digit = 15;
+        } //проверка условий для цифр 'A', 'B' ... 'F' и последующий их перевод для 10ой СС
+        else {
+            digit = do_zap[i] - '0'; //преобразование знака из char в int
+        }
+        result1 += digit * stepen1;
+        stepen1 *= 16;
+    }
+
+    double result2 = 0;
+    double stepen2 = 0.0625;
+    for (int i = 0; i < posle_zap.length(); i++) {
+        int digit;
+        if (posle_zap[i] == 'A') {
+            digit = 10;
+        }
+        else if (posle_zap[i] == 'B') {
+            digit = 11;
+        }
+        else if (posle_zap[i] == 'C') {
+            digit = 12;
+        }
+        else if (posle_zap[i] == 'D') {
+            digit = 13;
+        }
+        else if (posle_zap[i] == 'E') {
+            digit = 14;
+        }
+        else if (posle_zap[i] == 'F') {
+            digit = 15;
+        }
+        else {
+            digit = posle_zap[i] - '0';
+        }
+        result2 += digit * stepen2;
+        stepen2 /= 16;
+    }
+    int temp = result2 * pow(10, posle_zap.length());
+    string hex_dec; //окончательный результат перевода из 16ой в 10ую
+    if (result2 == 0) {
+        hex_dec = to_string(result1);
+    }
+    else if (result1 == 0) {
+        hex_dec = "0." + to_string(temp);
+    }
+    else {
+        hex_dec = to_string(result1) + "." + to_string(temp);
+    }
+    return hex_dec;
+}
+
+int main() {
+    string number;
+    int CI;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число: ";
+    cin >> number;
+    cout << "Введите систему счисления числа: ";
+    cin >> CI;
+    if (CI == 2) {
+        string celaya = do_zapyat(number);
+        string drobnaya = posle_zapyat(number);
+        cout << "Число в десятичной СС " << bin_to_dec(celaya, drobnaya);
+    } //если вводимое число в 2ой СС
+    else if (CI == 8) {
+        string celaya = do_zapyat(number);
+        string drobnaya = posle_zapyat(number);
+        cout << "Число в десятичной СС " << oct_to_dec(celaya, drobnaya);
+    } //если вводимое число в 8ой СС
+    else if (CI == 16) {
+        string celaya = do_zapyat(number);
+        string drobnaya = posle_zapyat(number);
+        cout << "Число в десятичной СС " << hex_to_dec(celaya, drobnaya);
+    } //если вводимое число в 16ой СС
+    else {
+        cout << "Такая система счисления не предусмотрена";
+    }
+}
